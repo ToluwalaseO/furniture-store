dc94944423f92b02bfc0ba1b5c13d1ee56c01015
ToluwalaseO <benjamoseni@gmail.com>
2025-01-23 02:03:56 -0700

    done2

--- src/TopSection.js
+++ src/TopSection.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./TopSection.css";
+import chair3 from './Assets/chair3.jpg'
+import chair4 from './Assets/chair4.jpg'
+import chair5 from './Assets/chair5.jpg'
 
 const TopSection = () => {
   return (
     <div className="top-section">
-      <div className="image-card large">
-        {/* <img src="/path-to-image1.jpg" alt="Sectional Sofas" /> */}
+      <div className="image-card1">
+        <img src={chair4} alt="topsection image" />
         <div className="overlay">
           <h2>Shop Sectionals</h2>
           <button className="btn-green">See Collection</button>
         </div>
       </div>
-      <div className="image-card small">
-        {/* <img src="/path-to-image2.jpg" alt="Rochester Silverware" /> */}
+      <div className="image-card2">
+        <img src={chair5} alt="Design Desk" />
         <h3>New in Rochester silverware</h3>
       </div>
-      <div className="image-card small">
-        {/* <img src="/path-to-image3.jpg" alt="Cosy Layers" /> */}
+      <div className="image-card3">
+        <img src={chair3} alt="topsection image" />
         <h3>Cosy Layers</h3>
       </div>
     </div>
